40f1e8b93dd49f8cfdf42a07e3d72508b329f151
Shivanshbyte <134778102+Shivanshbyte@users.noreply.github.com>
2024-08-09 00:58:08 +0530

    Time: 12 ms (65.77%), Space: 16.3 MB (33.34%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,56 +9,22 @@
  */
 class Solution {
 public:
-    bool check(TreeNode* root, TreeNode* p,TreeNode* q)
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if(root==NULL|| root==p || root==q)
     {
-        if(root==NULL)
-        {
-            return false;
-        }
-        if(root==p || root == q)
-        {
-            return true;
-        }
-        bool left=check(root->left,p,q);
-        bool right=check(root->right,p,q);
-        if( left || right )
-        {
-            return true;
-        }
-        return false;
+        return root;
     }
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
+    TreeNode * leftLCA=lowestCommonAncestor(root->left,p,q);
+    TreeNode* rightLCA=lowestCommonAncestor(root->right,p,q);
+    if(leftLCA && rightLCA)
     {
-        TreeNode* ans;
-        
-        bool left=check(root->left,p,q);
-        bool right=check(root->right,p,q);
-        if(left && right)
-        {
-            return root;
-        }
-        else if(left)
-        {
-            if(root==p || root==q)
-        {
-            return root;
-        }
-            ans=lowestCommonAncestor(root->left,p,q);
-        }
-        else if(right)
-        {
-            if(root==p || root==q)
-        {
-            return root;
-        }
-            ans=lowestCommonAncestor(root->right,p,q);
-        }
-        else
-            return nullptr;
-        
-        return ans;
+        return root;
+    }
+    if(leftLCA)
+    {
+        return leftLCA;
+    }
+    return rightLCA;
             
-    
-        
     }
 };
