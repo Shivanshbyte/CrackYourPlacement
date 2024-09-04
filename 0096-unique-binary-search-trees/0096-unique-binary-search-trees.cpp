989370c4509c71ff8e9ae7ffd9115b750cc3b91a
Shivanshbyte <134778102+Shivanshbyte@users.noreply.github.com>
2024-09-05 02:01:23 +0530

    Time: 2679 ms (5.05%), Space: 7.4 MB (75.84%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
-    
+    int dp[20+1];
+    int catalan(int n)
+    {
+        if(n==0 || n==1)
+        {
+            dp[n]=1;
+            return dp[n];
+        }
+        if(dp[n]!=0)
+        {
+            return dp[n];
+        }
+        int result=0;
+        for(int i=0;i<=n-1;i++)
+        {
+            result+=catalan(i)*catalan(n-i-1);
+        }
+        return result;
+    }
     
     int numTrees(int n) {
-    if(n==0 || n==1)
+    
+    for(int i=0;i<=n;i++)
     {
-        return 1;
+        dp[i]=0;
     }
-    int res=0;
-        
-    for(int i=0;i<=n-1;i++)
-    {
-        res+=numTrees(i)*numTrees(n-i-1);
-        
-    }
-    return res;
+    return catalan(n);
         
         
     }
