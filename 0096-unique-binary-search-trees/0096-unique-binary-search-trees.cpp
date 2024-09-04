1ffae74749f8d6103a1d9423f8165d6a5c9b990e
Shivanshbyte <134778102+Shivanshbyte@users.noreply.github.com>
2024-09-05 01:53:31 +0530

    Time: 2247 ms (5.05%), Space: 7.3 MB (75.84%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    
+    
+    int numTrees(int n) {
+    if(n==0 || n==1)
+    {
+        return 1;
+    }
+    int res=0;
+        
+    for(int i=0;i<=n-1;i++)
+    {
+        res+=numTrees(i)*numTrees(n-i-1);
+        
+    }
+    return res;
+        
+        
+    }
+};
